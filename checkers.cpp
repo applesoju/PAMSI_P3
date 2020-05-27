99540a88421927c5a5dbfa7efd602b74b603b6eb
applesoju <62670283+applesoju@users.noreply.github.com>
2020-05-28 00:30:42 +0200

    Add files via upload

--- checkers.cpp
+++ checkers.cpp
@@ -162,23 +162,9 @@ void Checkers::gameLoopForDriver() {
 	board.board[2][3] = board.board[2][5] = board.board[2][7] = board.board[2][9] = whiteM;
 	board.board[0][9] = whiteK;
 	board.board[7][2] = board.board[7][4] = board.board[7][6] = board.board[7][8] = blackM;
-	board.board[9][1] = blackK;
+	board.board[9][0] = blackK;
 
-	if (gameStart()) {				// jeœli  gra zostanie zaczêta poprzez wciœniêcie "u"
-		while (window.isOpen()) {	// dopóki okno jest otwarte
-			drawGameContent();		// rysuj elementy gry
-			window.display();
-
-			if (player) {			// jeœli jest teraz ruch gracza
-				movePlayer();		// wykonaj ruch
-			}
-			else moveAi();			// jeœli jest ruch ai, wykonaj ruch ai
-
-			player = !player;		// zmieñ to, czyja jest tura
-
-			if (gameOver(board.checkGameState(player))) return;	// sprawdŸ, czy gra siê zakoñczy³a
-		}
-	}
+	gameLoop();
 }
 
 bool Checkers::closeWindow() {
